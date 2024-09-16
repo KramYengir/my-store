b01395ffc7e0af49d3bf330a0f4a2dbf4a410762
KramYengir <kramyengir@gmail.com>
2024-09-16 21:49:53 +0100

    add apic key to script

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,7 +20,10 @@ export default function RootLayout({ children }: { children: ReactNode }) {
           rel="stylesheet"
           href="https://cdn.snipcart.com/themes/v3.0.31/default/snipcart.css"
         />
-        <script src="https://cdn.snipcart.com/themes/v3.0.31/default/snipcart.js"></script>
+        <script
+          src="https://cdn.snipcart.com/themes/v3.0.31/default/snipcart.js"
+          data-api-key={snipcartKey}
+        ></script>
       </head>
       <body className={`${inter.className}`}>
         {children}
