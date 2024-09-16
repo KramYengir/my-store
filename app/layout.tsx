ef73bf44ac7de52f17e29719eef1e75fe7644a29
KramYengir <kramyengir@gmail.com>
2024-09-16 21:59:20 +0100

    add api key directly for testing

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,13 @@
-// app/layout.tsx
 import "./globals.css";
 import { ReactNode } from "react";
 import { Inter } from "next/font/google";
 
 const inter = Inter({ subsets: ["latin"] });
 
+// Get the Snipcart API key from the environment variable
 const snipcartKey = process.env.NEXT_PUBLIC_SNIPCART_API_KEY;
+
+console.log("Snipcart API Key:", snipcartKey); // Add this to check if the key is correctly loaded
 
 export const metadata = {
   title: "Costa Climbing Store",
@@ -22,12 +24,20 @@ export default function RootLayout({ children }: { children: ReactNode }) {
         />
         <script
           src="https://cdn.snipcart.com/themes/v3.0.31/default/snipcart.js"
-          data-api-key={snipcartKey}
+          data-api-key={
+            "ZjcwMzhmODktY2Q3NC00OWViLTgwYzQtMWJlYWQ0NDViODNjNjM4NjIwODkzMTU2NjE2MTg4"
+          }
+          async
         ></script>
       </head>
-      <body className={`${inter.className}`}>
+      <body className={inter.className}>
         {children}
-        <div id="snipcart" data-api-key={snipcartKey}></div>
+        <div
+          id="snipcart"
+          data-api-key={
+            "ZjcwMzhmODktY2Q3NC00OWViLTgwYzQtMWJlYWQ0NDViODNjNjM4NjIwODkzMTU2NjE2MTg4"
+          }
+        ></div>
       </body>
     </html>
   );
